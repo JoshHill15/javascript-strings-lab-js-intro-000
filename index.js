ba0e2d38b1a438af52912161867c87737d4c5a24
Joshua Hill <josh_hill15@me.com>
2019-06-25 02:09:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,8 +8,10 @@ var topic = "space";
 
 var conversation = `${topic}`;
 
-var greeting = "hello, everybody!";
+var greeting = "Hello, everybody!";
+
 var greetSpecialGuest = "Hello, Neil deGrasse Tyson!";
 var topic = "Let's talk about space"
 topic
 console.greeting
+topic
